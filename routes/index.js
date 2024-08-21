246c10e86b92cbe2c4214d7ddf5daf998846d736
Gustavo Alexandre Cardoso de Sousa <gustavomac@MacBook-Pro-de-Gustavo.local>
2024-08-20 21:53:57 -0300

    main: initializing express server

---
+++ routes/index.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const defaultRoute = Router();
+
+defaultRoute.get("/", (req, res) => {});
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from "express";
 const server = express();
 const port = 3000;
 
+server.use("/");
+
 server.listen(port, () => {
   console.log("app listening on port: " + port);
 });
